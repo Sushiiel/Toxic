f869866c8741405c00e755df92e09a33e48b0878
Sushiiel Anand <132487769+Sushiiel@users.noreply.github.com>
2025-05-13 20:37:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Update and install unzip + Chrome
+# Update and install required tools
 apt-get update && apt-get install -y wget unzip curl gnupg
 
-# Install Google Chrome (headless)
+# Install Google Chrome (Headless version)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
 
-# Download and unzip ChromeDriver (version 136.0.7103.92 for Linux)
-mkdir -p /usr/local/bin/chromedriver
-cd /usr/local/bin/chromedriver
+# Download and unzip ChromeDriver (Linux version)
 wget https://storage.googleapis.com/chrome-for-testing-public/136.0.7103.92/linux64/chromedriver-linux64.zip
-unzip chromedriver-linux64.zip
-mv chromedriver /usr/local/bin/chromedriver
-chmod +x /usr/local/bin/chromedriver
+unzip chromedriver-linux64.zip -d /usr/local/bin/chromedriver
+
+# Set the proper permissions
+chmod +x /usr/local/bin/chromedriver/chromedriver
+
